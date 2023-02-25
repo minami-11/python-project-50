20e2a7d32f39de60fb03c2e5a119d93794abba58
minami-11 <madk0te@yandex.ru>
2023-02-25 21:49:46 +0500

    Fix: script gendiff add print in main()

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -13,7 +13,7 @@ args = parser.parse_args()
 
 
 def main():
-    generate_diff(args.first_file, args.second_file)
+    print(generate_diff(args.first_file, args.second_file))
 
 
 if __name__ == '__main__':
