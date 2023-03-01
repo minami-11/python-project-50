30a7478e6836f7bbc9393da3ffe1ce81f85ae0cc
minami-11 <madk0te@yandex.ru>
2023-03-01 12:49:25 +0500

    Refactor: gendiff.py v.0.1
    Add import from os.
    
    Rename funcs and variables, update docstrings.
    
    Fix errors with open empty json files.

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -1,17 +1,22 @@
 import json
+from os import SEEK_SET
 
 
 def get_json_from_file(file_1: str, file_2: str):
-    '''Return data from two json files'''
-    with open(file_1, 'r') as f_1:
-        with open(file_2, 'r') as f_2:
-            data_1 = json.load(f_1)
-            data_2 = json.load(f_2)
+    '''Return data from two json files if files are not empty'''
+    with open(file_1, 'r') as f1:
+        with open(file_2, 'r') as f2:
+            data = f1.read()
+            f1.seek(SEEK_SET, 0)
+            data_1 = json.load(f1) if data else {}
+            data = f2.read()
+            f2.seek(SEEK_SET, 0)
+            data_2 = json.load(f2) if data else {}
     return data_1, data_2
 
 
 def normalize(value):
-    '''Bring value to normal'''
+    '''Bring value to the required parameters'''
     converter = {
         'True': 'true',
         'False': 'false',
@@ -20,33 +25,35 @@ def normalize(value):
     return converter[str(value)] if str(value) in converter else value
 
 
-def match_difference(origin, sample_1, sample_2) -> list:
-    final_list = []
+def match_difference(merged, sample_1, sample_2) -> list:
+    output = []
 
-    for key, value in sorted(origin.items()):
+    for key, value in sorted(merged.items()):
         if key in sample_2 and key not in sample_1:
-            final_list.append(f'  + {key}: {normalize(value)}')
+            output.append(f'  + {key}: {normalize(value)}')
         elif key in sample_2 and value == sample_2.get(key):
-            final_list.append(f'    {key}: {normalize(value)}')
+            output.append(f'    {key}: {normalize(value)}')
         elif key in sample_2 and value != sample_2.get(key):
-            final_list.append(f'  - {key}: {normalize(value)}')
-            final_list.append(f'  + {key}: {normalize(sample_2.get(key))}')
+            output.append(f'  - {key}: {normalize(value)}')
+            output.append(f'  + {key}: {normalize(sample_2.get(key))}')
         elif key in sample_1 and key not in sample_2:
-            final_list.append(f'  - {key}: {normalize(value)}')
+            output.append(f'  - {key}: {normalize(value)}')
 
-    return final_list
+    return output
 
 
-def generate_diff(file_path_1: str, file_path_2: str):
+def generate_diff(file_path_1: str, file_path_2: str) -> None:
     '''Return differences between to files'''
+
     data1, data2 = get_json_from_file(file_path_1, file_path_2)
-    list_start = ['{']
-    list_end = ['}']
-    new_data = new_data = dict(data2 | data1)
-    result_list = list_start + match_difference(new_data,
-                                                data1, data2) + list_end
+    string_start = '{'
+    string_end = '}'
+    merged_data = dict(data2 | data1)
+    diff_string = '\n'.join(match_difference(merged_data, data1, data2))
 
-    # with open('result.json', 'w') as result_file:
-    #     json.dump(new_data, result_file, indent=3)
-
-    return '\n'.join(result_list)
+    with open('result.txt', 'w') as txt:
+        if diff_string:
+            print(string_start, diff_string, string_end, file=txt, sep='\n')
+        else:
+            print(string_start.strip() + diff_string + string_end.strip(),
+                  file=txt)
