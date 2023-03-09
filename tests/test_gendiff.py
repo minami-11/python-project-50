8577fcad9b1d94a32585909ec9834bc45961f395
minami-11 <madk0te@yandex.ru>
2023-03-09 14:34:03 +0500

    Update: test_gendiff.py add new fixtures, rename paths

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -8,20 +8,31 @@ path1 = Path(Path.cwd(), 'tests', 'fixtures', 'file0_1.json')
 path2 = Path(Path.cwd(), 'tests', 'fixtures', 'file0_2.json')
 path3 = Path(Path.cwd(), 'tests', 'fixtures', 'file1.json')
 path4 = Path(Path.cwd(), 'tests', 'fixtures', 'file2.json')
-path5 = Path(Path.cwd(), 'tests', 'fixtures', 'FILE0.yml')
-path6 = Path(Path.cwd(), 'tests', 'fixtures', 'FILE1.yaml')
-path7 = Path(Path.cwd(), 'tests', 'fixtures', 'FILE2.yml')
+path5 = Path(Path.cwd(), 'tests', 'fixtures', 'file3.json')
+path6 = Path(Path.cwd(), 'tests', 'fixtures', 'file4.json')
+
+
+path11 = Path(Path.cwd(), 'tests', 'fixtures', 'FILE0.yml')
+path12 = Path(Path.cwd(), 'tests', 'fixtures', 'FILE1.yaml')
+path13 = Path(Path.cwd(), 'tests', 'fixtures', 'FILE2.yml')
+path14 = Path(Path.cwd(), 'tests', 'fixtures', 'FILE3.yaml')
+path15 = Path(Path.cwd(), 'tests', 'fixtures', 'FILE4.yml')
+
 standart1 = Path(Path.cwd(), 'tests', 'fixtures', 'standart1.txt')
 standart2 = Path(Path.cwd(), 'tests', 'fixtures', 'standart2.txt')
 standart3 = Path(Path.cwd(), 'tests', 'fixtures', 'standart3.txt')
+standart4 = Path(Path.cwd(), 'tests', 'fixtures', 'standart4.txt')
 
 
 @pytest.mark.parametrize("file_1, file_2, correct_result", [
     (path1, path2, standart1),
     (path1, path3, standart2),
     (path3, path4, standart3),
-    (path5, path6, standart2),
-    (path6, path7, standart3)
+    (path11, path12, standart2),
+    (path12, path13, standart3),
+
+    (path5, path6, standart4),
+    (path14, path15, standart4)
 ])
 def test_gendiff_with_flat_json(file_1, file_2, correct_result):
     generate_diff(file_1, file_2)
