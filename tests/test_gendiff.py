4db4f40f9f692230d3f74545f36c874773c8d36c
minami-11 <madk0te@yandex.ru>
2023-03-11 16:49:46 +0500

    Feat and update: test_gendiff.py add new fixtures
    Add 3 new result fixtures for test with 'plain' format.
    
    Refactor test_gendiff.py. Add paths for fixtures. Split and rename main
    test function

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -4,23 +4,30 @@ from gendiff.gendiff import generate_diff
 
 
 gendiff_output = Path(Path.cwd(), 'result.txt')
-path1 = Path(Path.cwd(), 'tests', 'fixtures', 'file0_1.json')
-path2 = Path(Path.cwd(), 'tests', 'fixtures', 'file0_2.json')
-path3 = Path(Path.cwd(), 'tests', 'fixtures', 'file1.json')
-path4 = Path(Path.cwd(), 'tests', 'fixtures', 'file2.json')
-path5 = Path(Path.cwd(), 'tests', 'fixtures', 'file3.json')
-path6 = Path(Path.cwd(), 'tests', 'fixtures', 'file4.json')
 
-path11 = Path(Path.cwd(), 'tests', 'fixtures', 'FILE0.yml')
-path12 = Path(Path.cwd(), 'tests', 'fixtures', 'FILE1.yaml')
-path13 = Path(Path.cwd(), 'tests', 'fixtures', 'FILE2.yml')
-path14 = Path(Path.cwd(), 'tests', 'fixtures', 'FILE3.yaml')
-path15 = Path(Path.cwd(), 'tests', 'fixtures', 'FILE4.yml')
+# Test files fixtures:
+path1 = Path(Path.cwd(), 'tests', 'fixtures', 'file0_1.json')       # Empty
+path2 = Path(Path.cwd(), 'tests', 'fixtures', 'file0_2.json')       # {}
+path3 = Path(Path.cwd(), 'tests', 'fixtures', 'file1.json')         # Flat
+path4 = Path(Path.cwd(), 'tests', 'fixtures', 'file2.json')         # Flat
+path5 = Path(Path.cwd(), 'tests', 'fixtures', 'file3.json')         # Nested
+path6 = Path(Path.cwd(), 'tests', 'fixtures', 'file4.json')         # Nested
 
-standart1 = Path(Path.cwd(), 'tests', 'fixtures', 'standart1.txt')
-standart2 = Path(Path.cwd(), 'tests', 'fixtures', 'standart2.txt')
-standart3 = Path(Path.cwd(), 'tests', 'fixtures', 'standart3.txt')
-standart4 = Path(Path.cwd(), 'tests', 'fixtures', 'standart4.txt')
+path11 = Path(Path.cwd(), 'tests', 'fixtures', 'FILE0.yml')         # Empty
+path12 = Path(Path.cwd(), 'tests', 'fixtures', 'FILE1.yaml')        # Flat
+path13 = Path(Path.cwd(), 'tests', 'fixtures', 'FILE2.yml')         # Flat
+path14 = Path(Path.cwd(), 'tests', 'fixtures', 'FILE3.yaml')        # Nested
+path15 = Path(Path.cwd(), 'tests', 'fixtures', 'FILE4.yml')         # Nested
+
+# Result fixtures:
+standart1 = Path(Path.cwd(), 'tests', 'fixtures', 'standart1.txt')  # Empty
+standart2 = Path(Path.cwd(), 'tests', 'fixtures', 'standart2.txt')  # Empty|Flat
+standart3 = Path(Path.cwd(), 'tests', 'fixtures', 'standart3.txt')  # Flat|Flat
+standart4 = Path(Path.cwd(), 'tests', 'fixtures', 'standart4.txt')  # Nested
+
+plain2 = Path(Path.cwd(), 'tests', 'fixtures', 'plain2.txt')        # Empty|Flat
+plain3 = Path(Path.cwd(), 'tests', 'fixtures', 'plain3.txt')        # Flat|Flat
+plain4 = Path(Path.cwd(), 'tests', 'fixtures', 'plain4.txt')        # Nested
 
 
 @pytest.mark.parametrize("file_1, file_2, correct_result", [
@@ -28,12 +35,34 @@ standart4 = Path(Path.cwd(), 'tests', 'fixtures', 'standart4.txt')
     (path1, path3, standart2),
     (path3, path4, standart3),
     (path11, path12, standart2),
-    (path12, path13, standart3),
+    (path12, path13, standart3)
+])
+def test_gendiff_with_flat_files(file_1, file_2, correct_result):
+    generate_diff(file_1, file_2)
+    with open(gendiff_output, 'r') as output:
+        with open(correct_result, 'r') as correct:
+            assert output.read() == correct.read()
+
+
+@pytest.mark.parametrize("file_1, file_2, correct_result", [
     (path5, path6, standart4),
     (path14, path15, standart4)
 ])
-def test_gendiff_with_flat_json(file_1, file_2, correct_result):
+def test_gendiff_with_nested_files(file_1, file_2, correct_result):
     generate_diff(file_1, file_2)
+    with open(gendiff_output, 'r') as output:
+        with open(correct_result, 'r') as correct:
+            assert output.read() == correct.read()
+
+
+@pytest.mark.parametrize("file_1, file_2, correct_result", [
+    (path1, path2, standart1),
+    (path1, path3, plain2),
+    (path3, path4, plain3),
+    (path5, path6, plain4)
+])
+def test_gendiff_plain_format(file_1, file_2, correct_result):
+    generate_diff(file_1, file_2, 'plain')
     with open(gendiff_output, 'r') as output:
         with open(correct_result, 'r') as correct:
             assert output.read() == correct.read()
