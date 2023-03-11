70eacae7fe893113cfd63bfbe95713b616b2bf9d
minami-11 <madk0te@yandex.ru>
2023-03-11 17:00:01 +0500

    Feat: gendiff script add flag for format choosing

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -8,13 +8,15 @@ parser = argparse.ArgumentParser(description="Compares two configuration\
                                               files and shows a difference.")
 parser.add_argument('first_file', type=str)
 parser.add_argument('second_file', type=str)
-parser.add_argument('-f', '--format', help="set format of output")
+parser.add_argument('-f', '--format', type=str,
+                    help="output format (default: 'stylish')",
+                    default="stylish")
 
 args = parser.parse_args()
 
 
 def main():
-    generate_diff(args.first_file, args.second_file)
+    generate_diff(args.first_file, args.second_file, args.format)
     with open('result.txt', 'r') as file:
         result = file.read()
     print(result)
