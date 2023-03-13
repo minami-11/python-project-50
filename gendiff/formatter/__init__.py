6e4e3f49e488fee1b582ac448133eaadcd589fd9
minami-11 <madk0te@yandex.ru>
2023-03-13 23:49:39 +0500

    Fix: __init__.py in /formatter and small in gendiff.py

--- gendiff/formatter/__init__.py
+++ gendiff/formatter/__init__.py
@@ -1,7 +1,6 @@
-
 from ..formatter.stylish import get_shift
 from ..formatter.stylish import is_dict
-from..formatter.stylish import use_stylish_logic as use_stylish
+from ..formatter.stylish import use_stylish_logic as use_stylish
 
 from ..formatter.plain import use_plain_logic as use_plain
 
