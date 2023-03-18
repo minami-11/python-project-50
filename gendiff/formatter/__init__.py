424148e9b846004261d8bcc490c64bab8ccf1ffa
minami-11 <madk0te@yandex.ru>
2023-03-18 14:02:02 +0500

    Refactor: formatter/__init__.py v.0.5

--- gendiff/formatter/__init__.py
+++ gendiff/formatter/__init__.py
@@ -1,8 +1,5 @@
-from ..formatter.stylish import get_shift
-from ..formatter.stylish import is_dict
-from ..formatter.stylish import use_stylish_logic as use_stylish
-
-from ..formatter.plain import use_plain_logic as use_plain
+from ..formatter.stylish import use_stylish_format
+from ..formatter.plain import use_plain_format
 
 
-__all__ = ['is_dict', 'get_shift', 'use_stylish', 'use_plain']
+__all__ = ['use_stylish_format', 'use_plain_format']
