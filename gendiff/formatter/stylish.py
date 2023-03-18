ef69004285a925e5b5062386edc5e64e22e7181d
minami-11 <madk0te@yandex.ru>
2023-03-18 16:27:12 +0500

    Refactor, style: stylish.py
    Move common functions to format_libr.py. Add imports from format_libr.
    
    Add make_shift_line function. Rename get_finish_line to normalize.

--- gendiff/formatter/stylish.py
+++ gendiff/formatter/stylish.py
@@ -1,27 +1,24 @@
-def is_dict(value) -> bool:
-    """Return True if value is dictionary, else False"""
-    return isinstance(value, dict)
+from ..formatter.format_libr import is_dict, is_list, shift, get_change_mark
+from ..formatter.format_libr import get_node_value as node_value
 
 
-def shift(deep, variable: int = 2):
-    return ' ' * (4 * deep - variable)
+def make_shift_line(item: dict, mark, func=(lambda x: x)):
+    if mark == '-+':
+        return [f"{shift(item['depth'])}- {item['name']}: "
+                f"{func(node_value(item)[0])}",
+                f"{shift(item['depth'])}+ {item['name']}: "
+                f"{func(node_value(item)[1])}"
+                ]
+    return f"{shift(item['depth'])}{mark}" \
+           f"{item['name']}: {func(node_value(item))}"
 
 
-def get_change_mark(value) -> str:
-    '''Return modified mark if value is node'''
-    if is_dict(value):
-        return value['modified']
-    else:
-        return ''
-
-
-def get_finish_line(income):
+def normalize(income):
     output = []
     if not isinstance(income, (dict, list)):
         return income
-    if is_dict(income) and not isinstance(income['children'], (list, dict)):
-        output.append(f"{shift(income['depth'])}{income['modified']}"
-                      f"{income['name']}: {income['children']}")
+    if is_dict(income) and not isinstance(node_value(income), (list, dict)):
+        output.append(make_shift_line(income, get_change_mark(income)))
     else:
         output.append(use_stylish_format(income))
     return '\n'.join(output)
@@ -36,18 +33,12 @@ def use_stylish_format(income_list) -> str:
     for item in income_list:
         mark = get_change_mark(item)
 
-        if all([is_dict(item), isinstance(item['children'], list),
-                mark == "-+"]):
-            output.append(f"{shift(item['depth'])}- {item['name']}: "
-                          f"{get_finish_line(item['children'][0])}")
-            output.append(f"{shift(item['depth'])}+ {item['name']}: "
-                          f"{get_finish_line(item['children'][1])}")
-
-        elif is_dict(item) and isinstance(item['children'], list):
-            output.append(f"{shift(item['depth'])}{mark}{item['name']}: "
-                          f"{use_stylish_format(item['children'])}")
+        if all([is_dict(item), is_list(node_value(item)), mark == "-+"]):
+            output.extend(make_shift_line(item, mark, normalize))
+        elif is_dict(item) and is_list(node_value(item)):
+            output.append(make_shift_line(item, mark, use_stylish_format))
         else:
-            output.append(f"{get_finish_line(item)}")
+            output.append(f"{normalize(item)}")
 
     output.append(f"{shift(item['depth'] - 1, 0)}" + '}')
     return '\n'.join(map(lambda x: x.rstrip(), output))
