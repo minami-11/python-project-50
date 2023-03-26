23776fd8fbe1d6cb8372239ad336dc5e8199fb0e
minami-11 <madk0te@yandex.ru>
2023-03-26 18:02:31 +0500

    Refactor and update: stylish.py due to v.1.0
    Due to total refactoring v.1.0: after changing the logic and appearance
    of the output raw data - stylish logic was also refactored.

--- gendiff/formatter/stylish.py
+++ gendiff/formatter/stylish.py
@@ -1,44 +1,51 @@
-from ..formatter.format_libr import is_dict, is_list, shift, get_change_mark
-from ..formatter.format_libr import get_node_value as node_value
+from ..formatter.format_libr import is_dict, shift
+from ..formatter.format_libr import get_node_value
+from ..formatter.format_libr import get_node_type
+from ..formatter.format_libr import pass_through_converter as convert
 
 
-def make_shift_line(item: dict, mark, func=(lambda x: x)):
-    if mark == '-+':
-        return [f"{shift(item['depth'])}- {item['name']}: "
-                f"{func(node_value(item)[0])}",
-                f"{shift(item['depth'])}+ {item['name']}: "
-                f"{func(node_value(item)[1])}"
-                ]
-    return f"{shift(item['depth'])}{mark}" \
-           f"{item['name']}: {func(node_value(item))}"
+def get_type_mark(income: dict):
+    '''Convert raw modified type to char mark'''
+    type_converter = {
+        'same': '  ',
+        'added': '+ ',
+        'removed': '- '
+    }
+    node_type = get_node_type(income)
+    return type_converter.get(node_type)
 
 
-def normalize(income):
-    output = []
-    if not isinstance(income, (dict, list)):
-        return income
-    if is_dict(income) and not isinstance(node_value(income), (list, dict)):
-        output.append(make_shift_line(income, get_change_mark(income)))
-    else:
-        output.append(use_stylish_format(income))
-    return '\n'.join(output)
+def make_valid(value, deep):
+    '''Make valid value from income data'''
+    if is_dict(value):
+        return use_stylish_format(value, deep)
+    valid_value = convert(value)
+    return valid_value
 
 
-def use_stylish_format(income_list) -> str:
-    '''Format input string in stylish way'''
-    if not income_list:
-        return '{}'
-    output = []
-    output.append('{')
-    for item in income_list:
-        mark = get_change_mark(item)
+def use_stylish_format(income: dict, deep: int = 0) -> str:
+    '''Format income raw data in stylish way'''
+    if not income:
+        return ''
+    deep += 1
+    output = ['{']
 
-        if all([is_dict(item), is_list(node_value(item)), mark == "-+"]):
-            output.extend(make_shift_line(item, mark, normalize))
-        elif is_dict(item) and is_list(node_value(item)):
-            output.append(make_shift_line(item, mark, use_stylish_format))
+    for key, value in income.items():
+        mark = get_type_mark(value)
+        marked_name = f"{shift(deep)}{mark}{key}"
+        node_value = get_node_value(value)
+        node_type = get_node_type(value)
+
+        if is_dict(node_value) and node_type != 'changed':
+            output.append(f"{marked_name}: "
+                          f"{use_stylish_format(node_value, deep)}")
+        elif node_type == 'changed':
+            output.append(f"{shift(deep)}- {key}: "
+                          f"{make_valid(node_value[0], deep)}")
+            output.append(f"{shift(deep)}+ {key}: "
+                          f"{make_valid(node_value[1], deep)}")
         else:
-            output.append(f"{normalize(item)}")
+            output.append(f"{marked_name}: {make_valid(node_value, deep)}")
 
-    output.append(f"{shift(item['depth'] - 1, 0)}" + '}')
+    output.append(f"{shift(deep - 1, 0)}" + '}')
     return '\n'.join(output)
