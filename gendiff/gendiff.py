3b765e8b7df08e77a10f75cdf7b201ceb88f9197
minami-11 <madk0te@yandex.ru>
2023-03-26 17:42:29 +0500

    Refactor and update: gendiff.py due to v.1.0
    Due to the total rafactoring v.1.0: add code logic part from previous
    parse module, responsible for finding the differences between 2 data sources
    and making raw data with diff marks.
    
    Move reading data from source to new module called: data_getter.

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -1,40 +1,82 @@
 import json
-import yaml
-from os import SEEK_SET
 
-from gendiff.parser import parse_for_differences
 from gendiff.formatter import use_stylish_format
 from gendiff.formatter import use_plain_format
+from gendiff.formatter import is_dict
+from gendiff.data_getter import get_data_from_source as get_data
 
 
-def get_data_from_file(file_path: str) -> dict:
-    '''Return data from not empty files (with correct file type)'''
-    with open(file_path, 'r') as file:
-        data = file.read()
-        file.seek(SEEK_SET, 0)
-        if file_path.endswith('.json'):
-            output_data = json.load(file) if data else {}
-        elif file_path.endswith('.yaml') or file_path.endswith('.yml'):
-            output_data = yaml.safe_load(file) if data else {}
+def process_nested_dict(sample):
+    '''Process single nested dict with adding change mark'''
+    sub_tree = {}
+    for key, value in sample.items():
+        sub_tree[key] = {'type': 'same'}
+        if is_dict(value):
+            sub_tree[key]['children'] = process_nested_dict(value)
         else:
-            raise TypeError('Get ERROR. Check the file type.')
-        return output_data
+            sub_tree[key]['children'] = value
+
+    return sub_tree
 
 
-def generate_diff(file_1: str, file_2: str, format: str = 'stylish') -> None:
+def add_modified_marks(data_1, data_2, key):
+    '''Add modified marks for each value change'''
+    value_1 = data_1.get(key)
+    valid_value1 = process_nested_dict(value_1) if is_dict(value_1) else value_1
+    value_2 = data_2.get(key)
+    valid_value2 = process_nested_dict(value_2) if is_dict(value_2) else value_2
+    sub_tree = {}
+
+    if key in data_2 and key not in data_1:
+        sub_tree['type'] = 'added'
+        sub_tree['children'] = valid_value2
+    elif key in data_2 and data_1.get(key) == data_2.get(key):
+        sub_tree['type'] = 'same'
+        sub_tree['children'] = valid_value2
+    elif key in data_2 and data_1.get(key) != data_2.get(key):
+        sub_tree['type'] = 'changed'
+        sub_tree['children'] = []
+        sub_tree['children'].append(valid_value1)
+        sub_tree['children'].append(valid_value2)
+    else:
+        sub_tree['type'] = 'removed'
+        sub_tree['children'] = valid_value1
+    return sub_tree
+
+
+def make_raw_variance(data_1: dict, data_2: dict):
+    '''Find is there differences between two dicts and return result as list'''
+    tree_dict = {}
+    keys_set = sorted(set(data_1) | set(data_2))
+
+    for key in keys_set:
+        value_1 = data_1.get(key)
+        value_2 = data_2.get(key)
+        if all(map(is_dict, [value_1, value_2])):
+            tree_dict[key] = {
+                'type': 'same',
+                'children': make_raw_variance(value_1, value_2)
+            }
+        else:
+            tree_dict[key] = add_modified_marks(data_1, data_2, key)
+
+    return tree_dict
+
+
+def generate_diff(source_1: str, source_2: str, format: str = 'stylish') -> str:
     '''Compare two files and match differences between them'''
 
-    dict_1, dict_2 = list(map(get_data_from_file, (str(file_1), str(file_2))))
-
-    diff_list = parse_for_differences(dict_1, dict_2)
+    parsed_data_1 = get_data(source_1)
+    parsed_data_2 = get_data(source_2)
+    raw_variance_data = make_raw_variance(parsed_data_1, parsed_data_2)
     match format:
-        case 'stylish': formatted_string = use_stylish_format(diff_list)
-        case 'plain': formatted_string = use_plain_format(diff_list)
-        case _:
+        case 'stylish': formatted_string = use_stylish_format(raw_variance_data)
+        case 'plain': formatted_string = use_plain_format(raw_variance_data)
+        case 'json':
             with open('result.json', 'w') as json_file:
-                json.dump(diff_list, json_file, indent=4)
-                formatted_string = json.dumps(diff_list)
-    if format != 'json':
-        with open('result.txt', 'w') as txt:
-            print(formatted_string, file=txt)
+                json.dump(raw_variance_data, json_file, indent=4)
+                formatted_string = json.dumps(raw_variance_data)
+        case _:
+            raise ValueError('Unknown format type, please check the format')
+
     return formatted_string
