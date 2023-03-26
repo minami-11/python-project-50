c638ae881120ac9cf251b0b2ebbee0014390d4da
minami-11 <madk0te@yandex.ru>
2023-03-26 17:33:15 +0500

    Refactor and update: formatter/__init__.py due to v.1.0
    Due to refactor v.1.0  add imports: is_dict, pass_through_converter.
    
    Update __all__ .

--- gendiff/formatter/__init__.py
+++ gendiff/formatter/__init__.py
@@ -1,5 +1,8 @@
 from ..formatter.stylish import use_stylish_format
 from ..formatter.plain import use_plain_format
+from ..formatter.format_libr import is_dict
+from ..formatter.format_libr import pass_through_converter
 
 
-__all__ = ['use_stylish_format', 'use_plain_format']
+__all__ = ['use_stylish_format', 'use_plain_format', 'is_dict',
+           'pass_through_converter']
