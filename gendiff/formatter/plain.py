f07b39a52f6b322269e8df9bb53a698264e48f69
minami-11 <madk0te@yandex.ru>
2023-03-29 21:20:20 +0500

    Update and style: plain.py due to v.2.0
    Rename format_libr module to format. Make_valid function rename to
    to_string.
    
    Use_plain_format rename to format. Income variable rename to diff.

--- gendiff/formatter/plain.py
+++ gendiff/formatter/plain.py
@@ -1,10 +1,10 @@
-from ..formatter.format_libr import is_dict
-from ..formatter.format_libr import get_node_value
-from ..formatter.format_libr import get_node_type
-from ..formatter.format_libr import pass_through_converter as convert
+from ..formatter.format import is_dict
+from ..formatter.format import get_node_value
+from ..formatter.format import get_node_type
+from ..formatter.format import pass_through_converter as convert
 
 
-def make_valid(value):
+def to_string(value):
     '''Bring value to the required output parameters'''
     if isinstance(value, (list, dict)):
         return "[complex value]"
@@ -14,27 +14,27 @@ def make_valid(value):
                                   isinstance(valid_v, str)]) else valid_v
 
 
-def get_valid_conclusion(item):
+def get_valid_conclusion(item) -> str:
     '''Return valid output conclusion'''
     node_value = get_node_value(item)
     node_type = get_node_type(item)
 
     match node_type:
         case 'added': return f"was added with value: " \
-                             f"{make_valid(node_value)}"
+                             f"{to_string(node_value)}"
         case 'removed': return 'was removed'
         case 'changed':
-            value_1 = make_valid(node_value[0])
-            value_2 = make_valid(node_value[1])
+            value_1 = to_string(node_value[0])
+            value_2 = to_string(node_value[1])
             return f"was updated. From {value_1} to {value_2}"
 
 
-def use_plain_format(income_dict: dict):
+def format(input_diff: dict) -> str:
     '''Format income raw data in plain way'''
 
-    def walk(income, path):
+    def walk(diff, path):
         output = []
-        for key, value in income.items():
+        for key, value in diff.items():
             node_value = get_node_value(value)
             node_type = get_node_type(value)
             point = '.' * (bool(path))
@@ -51,4 +51,4 @@ def use_plain_format(income_dict: dict):
                                   f"{get_valid_conclusion(value)}")
 
         return '\n'.join(filter(None, output))
-    return walk(income_dict, '')
+    return walk(input_diff, '')
